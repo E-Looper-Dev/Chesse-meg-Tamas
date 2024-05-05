f0b8e17495ee3d7627de22df350dfb584badf680
PatsFan12 <tamas.deme.andras12@gmail.com>
2024-05-05 13:17:22 +0200

    ForwardMoves & DiagonalMoves method

--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -28,5 +28,53 @@ namespace ChessLogic
             yield return new PawnPromotion(from, to PieceType.Rook);
             yield return new PawnPromotion(from, to PieceType.Queen);
         }
+        
+        private IEnumerable<Move> ForwardMoves(Position from,Board board)
+        {
+            Position oneMovePos = from + forward;
+            if (CanMoveTo(oneMovePos, board))
+            {
+                if (oneMovePos.Row == 0 || oneMovePos.Row == 7)
+                {
+                    foreach (Move promMove in PromotionMoves(from, oneMovesPos))
+                    {
+                        yield return promMove;
+                    }
+                }
+                else
+                {
+                    yield return new NormalMove(from, oneMovePos);
+                }
+
+                Position twoMovesPos = oneMovePos + forward;
+                
+                if (!HasMoved && CanMoveTo(twoMovesPos, board))
+                {
+                    yield return new NormalMove(from, twoMovesPos);
+                }
+            }
+        }
+
+        private IEnumerable<Move> DiagonalMoves(Position from, Board board)
+        {
+            foreach (Direction dir in new Direction[] { Direction.West, Direction.East })
+            {
+                Position to = from + forward + dir;
+                if (CanCaptureAt(to,board))
+                {
+                    if (to.Row == 0 || to.Row == 7)
+                    {
+                        foreach (Move promMove in PromotionMoves(from, to))
+                        {
+                            yield return promMove;
+                        }
+                    }
+                    else
+                    {
+                        yield return new NormalMove(from, to);
+                    }
+                }
+            }
+        }
     }
 }
